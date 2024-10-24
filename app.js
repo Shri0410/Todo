70df3f2a2f4d20bbf79f5974eccbd2d0ce7dcf32
Shri0410 <waghmareshrinivas007@gmail.com>
2024-10-24 11:19:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = inp.value;
+  let delbtn = document.createElement("button");
+  delbtn.innerText = "delete";
+  delbtn.classList.add("delete");
+
+  delbtn.addEventListener("click", function () {
+    ul.removeChild(item);
+  });
+
+  item.appendChild(delbtn);
+  ul.appendChild(item);
+  inp.value = "";
+});
